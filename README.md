bb155004ec5ef748140e2a5bab052cab05f8844f
Miroslav Dubovský <dubovsky@onnet.sk>
2017-12-30 21:37:10 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # oracleClientCore-Sample
 Small app for test .Net Core Oracle client.
+Used [oracleClientCore-2.0](https://github.com/ericmend/oracleClientCore-2.0) library
+([Nuget](https://www.nuget.org/packages/dotNetCore.Data.OracleClient))
+
+
+
+### Requrements
+
+* [Install .NET Core SDK 2.0](https://www.microsoft.com/net/download/core)
+* [Install OCI - Client Oracle](http://www.oracle.com/technetwork/database/features/instant-client/index-097480.html) or full Oracle client.
+
+* (linux) Unzip the files and declare the environment variables. eg: LD_LIBRARY_PATH="/opt/oracle/instantclient"; 
+  OCI_HOME="/opt/oracle/instantclient"; OCI_LIB_DIR="/opt/oracle/instantclient"; PATH=$PATH:"/opt/oracle/instantclient"
+* (windows) Unzip the files and declare the environment variables. eg: LD_LIBRARY_PATH="C:\instantclient_12_2\"; 
+  OCI_HOME="C:\instantclient_12_2\"; OCI_LIB_DIR="C:\instantclient_12_2\"; PATH=%PATH%;"C:\instantclient_12_2\"
+*  Set NLS_LANG settings, if it is not setteted correctly eg: in windows: SET NLS_LANG=SLOVAK_SLOVAKIA.EE8MSWIN1250 ( or set registry key), in Linux you can use: export NLS_LANG=SLOVAK_SLOVAKIA.UTF8 . 
+More resources: [NLS_LANG FAQ](http://www.oracle.com/technetwork/database/database-technologies/globalization/nls-lang-099431.html#_Toc110410555)
+
+
+### Note:
+
+Work has been done to work on Windows / Linux / MacOsX. Tested so far only Windows (Win10) and Linux (Ubuntu 17.10, CentOS 7).
---
+++ src/oracleClientCore-Sample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.OracleClient;
+
+namespace SimpleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start.");
+
+            Console.WriteLine("create OracleConnection object...");
+            //https://stackoverflow.com/questions/9218847/how-do-i-handle-database-connections-with-dapper-in-net
+            using (System.Data.Common.DbConnection connection = new OracleConnection("Data Source = RHEL5; User ID = TEST; Password = Passw0rd1 "))
+            {
+                Console.WriteLine("Open connection...");
+                connection.Open();
+
+                Console.WriteLine("Create command...");
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT table_name, tablespace_name, num_rows FROM user_tables  WHERE tablespace_name is not null  and num_rows > 0  order by num_rows desc";
+
+                    Console.WriteLine("Execute reader...");
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("*** User tables (sample): ***");
+
+                        while (reader.Read())
+                        {
+                            string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
+                            string tablespace_name = reader.GetString(reader.GetOrdinal("TABLESPACE_NAME"));
+                            int rows = reader.GetInt32(reader.GetOrdinal("NUM_ROWS"));
+                            Console.WriteLine(tableName + " in tablespace " + tablespace_name + " has " + rows.ToString() + " rows.");
+                        };
+                    }
+                    Console.WriteLine("End reader...");
+                    Console.WriteLine();
+
+                    Console.WriteLine("*** Test NLS_LANG settings ***");
+                    command.CommandText = "select 'some text in English language' as a, '储物组合带门/抽屉, 白色 卡维肯, 因维肯 白蜡木贴面' as b,  'ľščťžýáííéô§úä' as c from dual";
+                    var reader1 = command.ExecuteReader();
+                    reader1.Read();
+                    string sEnglish = reader1.GetString(0);
+                    string sChinese = reader1.GetString(1);
+                    string sSlovak = reader1.GetString(2);
+
+                    Console.WriteLine("English from db: " + sEnglish);
+                    Console.WriteLine("Chinese from db: " + sChinese);
+                    Console.WriteLine("Slovak  from db: " + sSlovak);
+                    Console.WriteLine("Chinese from the code: 储物组合带门 / 抽屉, 白色 卡维肯, 因维肯 白蜡木贴面");
+                    Console.WriteLine("Slovak  from the code: ľščťžýáííéô§úä");
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
