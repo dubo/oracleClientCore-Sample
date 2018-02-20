0fe2fd1d028b35e385ab89721d35dbe7ec5e8381
Miroslav Dubovský <dubovsky@onnet.sk>
2018-02-20 22:57:02 +0100

    Added Oracle.ManagedDataAccess library

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # oracleClientCore-Sample
 Small app for test .Net Core Oracle client.
-Based on [ericmend](https://github.com/ericmend)  [oracleClientCore-2.0](https://github.com/ericmend/oracleClientCore-2.0) library
-([Nuget](https://www.nuget.org/packages/dotNetCore.Data.OracleClient))
+### 1. dotNetCore.Data.OracleClient with Oci client .
+Based on [ericmend](https://github.com/ericmend)  [oracleClientCore-2.0](https://github.com/ericmend/oracleClientCore-2.0) library  ([Nuget](https://www.nuget.org/packages/dotNetCore.Data.OracleClient))
 
-
-
-### Requrements
+#### Requrements
 
 * [Install .NET Core SDK 2.0](https://www.microsoft.com/net/download/core)
 * [Install OCI - Client Oracle](http://www.oracle.com/technetwork/database/features/instant-client/index-097480.html) or full Oracle client.
@@ -14,10 +12,20 @@ Based on [ericmend](https://github.com/ericmend)  [oracleClientCore-2.0](https:/
   OCI_HOME="/opt/oracle/instantclient"; OCI_LIB_DIR="/opt/oracle/instantclient"; PATH=$PATH:"/opt/oracle/instantclient"
 * (windows) Unzip the files and declare the environment variables. eg: LD_LIBRARY_PATH="C:\instantclient_12_2\"; 
   OCI_HOME="C:\instantclient_12_2\"; OCI_LIB_DIR="C:\instantclient_12_2\"; PATH=%PATH%;"C:\instantclient_12_2\"
-*  Set NLS_LANG settings, if it is not setteted correctly eg: in windows: SET NLS_LANG=SLOVAK_SLOVAKIA.EE8MSWIN1250 ( or set registry key), in Linux you can use: export NLS_LANG=SLOVAK_SLOVAKIA.UTF8 . 
-More resources: [NLS_LANG FAQ](http://www.oracle.com/technetwork/database/database-technologies/globalization/nls-lang-099431.html#_Toc110410555)
+*  Set NLS_LANG settings, if it is not setteted correctly eg: in windows: SET NLS_LANG=SLOVAK_SLOVAKIA.EE8MSWIN1250 ( or set registry key), in Linux you can use: export NLS_LANG=SLOVAK_SLOVAKIA.UTF8 .      //Slovak
+NLS_LANG= BRAZILIAN PORTUGUESE_BRAZIL.WE8ISO8859P1 //Portugeese alt 1
+NLS_LANG= PORTUGUESE_PORTUGAL.WE8MSWIN1252  //Portugeese alt 2 and others
+More resources about possible values: [NLS_LANG FAQ](http://www.oracle.com/technetwork/database/database-technologies/globalization/nls-lang-099431.html#_Toc110410555)
 
 
-### Note:
+#### Note:
 
 Work has been done to work on Windows / Linux / MacOsX. Tested so far only Windows (Win10) and Linux (Ubuntu 17.10, CentOS 7).
+
+### 2. Oracle.ManagedDataAccess.Client  .
+#### Requrements
+* Download actual Oracle .Net Core library
+http://www.oracle.com/technetwork/topics/dotnet/downloads/net-downloads-160392.html 
+* Copy it to lib directory - sample:  c:\work\oracleClientCore-Sample\lib\..  and add as reference
+* Configure your conn string - possible samples in code - with or withouth tnsnames.ora
+* Configure NLS_LANG for language settins - see above 
--- src/oracleClientCore-Sample/Program.cs
+++ src/oracleClientCore-Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Data.OracleClient;
+//using System.Data.OracleClient;
+using Oracle.ManagedDataAccess.Client;
 
 namespace SimpleTest
 {
@@ -7,52 +8,117 @@ namespace SimpleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start.");
-
-            Console.WriteLine("create OracleConnection object...");
-            //https://stackoverflow.com/questions/9218847/how-do-i-handle-database-connections-with-dapper-in-net
-            using (System.Data.Common.DbConnection connection = new OracleConnection("Data Source = RHEL5; User ID = TEST; Password = Passw0rd1 "))
+            Console.WriteLine("1. Test dotNetCore.Data.OracleClient with native OCI client");
+            Console.WriteLine("2. Test Oracle.ManagedDataAccess.Client");
+            Console.WriteLine("Other - exit");
+            var key = Console.ReadKey().KeyChar;
+            if (key == '1')
             {
-                Console.WriteLine("Open connection...");
-                connection.Open();
+                Console.WriteLine("Test dotNetCore.Data.OracleClient with Oci client.");
 
-                Console.WriteLine("Create command...");
-                using (var command = connection.CreateCommand())
+                Console.WriteLine("create OracleConnection object...");
+                //https://stackoverflow.com/questions/9218847/how-do-i-handle-database-connections-with-dapper-in-net
+                using (System.Data.Common.DbConnection connection = new System.Data.OracleClient.OracleConnection("Data Source = RHEL5; User ID = TEST; Password = Passw0rd1 "))
                 {
-                    command.CommandText = "SELECT table_name, tablespace_name, num_rows FROM user_tables  WHERE tablespace_name is not null  and num_rows > 0  order by num_rows desc";
+                    Console.WriteLine("Open connection...");
+                    connection.Open();
 
-                    Console.WriteLine("Execute reader...");
-                    using (var reader = command.ExecuteReader())
+                    Console.WriteLine("Create command...");
+                    using (var command = connection.CreateCommand())
                     {
-                        Console.WriteLine("*** User tables (sample): ***");
+                        command.CommandText = "SELECT table_name, tablespace_name, num_rows FROM user_tables  WHERE tablespace_name is not null  and num_rows > 0  order by num_rows desc";
 
-                        while (reader.Read())
+                        Console.WriteLine("Execute reader...");
+                        using (var reader = command.ExecuteReader())
                         {
-                            string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
-                            string tablespace_name = reader.GetString(reader.GetOrdinal("TABLESPACE_NAME"));
-                            int rows = reader.GetInt32(reader.GetOrdinal("NUM_ROWS"));
-                            Console.WriteLine(tableName + " in tablespace " + tablespace_name + " has " + rows.ToString() + " rows.");
-                        };
+                            Console.WriteLine("*** User tables (sample): ***");
+
+                            while (reader.Read())
+                            {
+                                string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
+                                string tablespace_name = reader.GetString(reader.GetOrdinal("TABLESPACE_NAME"));
+                                int rows = reader.GetInt32(reader.GetOrdinal("NUM_ROWS"));
+                                Console.WriteLine(tableName + " in tablespace " + tablespace_name + " has " + rows.ToString() + " rows.");
+                            };
+                        }
+                        Console.WriteLine("End reader...");
+                        Console.WriteLine();
+
+                        Console.WriteLine("*** Test NLS_LANG settings ***");
+                        command.CommandText = "select 'some text in English language' as a, '储物组合带门/抽屉, 白色 卡维肯, 因维肯 白蜡木贴面' as b,  'ľščťžýáííéô§úä' as c from dual";
+                        var reader1 = command.ExecuteReader();
+                        reader1.Read();
+                        string sEnglish = reader1.GetString(0);
+                        string sChinese = reader1.GetString(1);
+                        string sSlovak = reader1.GetString(2);
+
+                        Console.WriteLine("English from db: " + sEnglish);
+                        Console.WriteLine("Chinese from db: " + sChinese);
+                        Console.WriteLine("Slovak  from db: " + sSlovak);
+                        Console.WriteLine("Chinese from the code: 储物组合带门 / 抽屉, 白色 卡维肯, 因维肯 白蜡木贴面");
+                        Console.WriteLine("Slovak  from the code: ľščťžýáííéô§úä");
                     }
-                    Console.WriteLine("End reader...");
-                    Console.WriteLine();
-
-                    Console.WriteLine("*** Test NLS_LANG settings ***");
-                    command.CommandText = "select 'some text in English language' as a, '储物组合带门/抽屉, 白色 卡维肯, 因维肯 白蜡木贴面' as b,  'ľščťžýáííéô§úä' as c from dual";
-                    var reader1 = command.ExecuteReader();
-                    reader1.Read();
-                    string sEnglish = reader1.GetString(0);
-                    string sChinese = reader1.GetString(1);
-                    string sSlovak = reader1.GetString(2);
-
-                    Console.WriteLine("English from db: " + sEnglish);
-                    Console.WriteLine("Chinese from db: " + sChinese);
-                    Console.WriteLine("Slovak  from db: " + sSlovak);
-                    Console.WriteLine("Chinese from the code: 储物组合带门 / 抽屉, 白色 卡维肯, 因维肯 白蜡木贴面");
-                    Console.WriteLine("Slovak  from the code: ľščťžýáííéô§úä");
                 }
+                Console.WriteLine("Done Oci Client");
+                Console.WriteLine("Press key to exit");
+                key = Console.ReadKey().KeyChar;
             }
-            Console.WriteLine("Done.");
+            else if (key == '2')
+            {
+                Console.WriteLine("Test Oracle.ManagedDataAccess.Client.");
+                Console.WriteLine("create OracleConnection object...");
+                //1. Full TNS string in connect string 
+                using (System.Data.Common.DbConnection connection = new OracleConnection("Data Source = (DESCRIPTION =  (ADDRESS_LIST =  (ADDRESS = (PROTOCOL = TCP)(HOST = 10.100.103.67)(PORT = 1521)) )(CONNECT_DATA = (SID = ORCL))); User ID = TEST; Password = Passw0rd1 "))
+                //2 Withouth TNS , with service name  =  " select value from v$parameter where name='service_names' "
+                // using (System.Data.Common.DbConnection connection = new OracleConnection("Data Source =  10.100.103.67:1521/orcl.test.sk; User ID = TEST; Password = Passw0rd1 "))
+                // With alias from TNS  
+                // using (OracleConnection connection = new OracleConnection(" User ID = TEST; Password = Passw0rd1; Data Source = RHEL5;"))
+                {
+
+                    Console.WriteLine("Open connection...");
+                    connection.Open();
+
+                    Console.WriteLine("Create command...");
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT table_name, tablespace_name, num_rows FROM user_tables  WHERE tablespace_name is not null  and num_rows > 0  order by num_rows desc";
+
+                        Console.WriteLine("Execute reader...");
+                        using (var reader = command.ExecuteReader())
+                        {
+                            Console.WriteLine("*** User tables (sample): ***");
+
+                            while (reader.Read())
+                            {
+                                string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
+                                string tablespace_name = reader.GetString(reader.GetOrdinal("TABLESPACE_NAME"));
+                                int rows = reader.GetInt32(reader.GetOrdinal("NUM_ROWS"));
+                                Console.WriteLine(tableName + " in tablespace " + tablespace_name + " has " + rows.ToString() + " rows.");
+                            };
+                        }
+                        Console.WriteLine("End reader...");
+                        Console.WriteLine();
+
+                        Console.WriteLine("*** Test NLS_LANG settings ***");
+                        command.CommandText = "select 'some text in English language' as a, '储物组合带门/抽屉, 白色 卡维肯, 因维肯 白蜡木贴面' as b,  'ľščťžýáííéô§úä' as c from dual";
+                        var reader1 = command.ExecuteReader();
+                        reader1.Read();
+                        string sEnglish = reader1.GetString(0);
+                        string sChinese = reader1.GetString(1);
+                        string sSlovak = reader1.GetString(2);
+
+                        Console.WriteLine("English from db: " + sEnglish);
+                        Console.WriteLine("Chinese from db: " + sChinese);
+                        Console.WriteLine("Slovak  from db: " + sSlovak);
+                        Console.WriteLine("Chinese from the code: 储物组合带门 / 抽屉, 白色 卡维肯, 因维肯 白蜡木贴面");
+                        Console.WriteLine("Slovak  from the code: ľščťžýáííéô§úä");
+                    }
+                }
+                Console.WriteLine("Done Oracle.ManagedDataAccess.Client");
+                Console.WriteLine("Press key to exit");
+                key = Console.ReadKey().KeyChar;
+            }
+            
         }
     }
 }
